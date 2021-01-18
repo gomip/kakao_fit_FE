182f818b67ff581902bd47eab9bff476350d96dc
gomip <rlawjdgns15@hotmail.com>
2021-01-18 23:27:22 +0900

    사용자 데이터 정보 변경

--- src/Info/UserData.tsx
+++ src/Info/UserData.tsx
@@ -8,26 +8,32 @@ import * as React from 'react'
 export const UserData = [
     {
         userId: '1',
-        userName: '하입'
+        // userName: '하입'
+        userName: 'data_1'
     },
     {
         userId: '2',
-        userName: '디안시'
+        // userName: '디안시'
+        userName: 'data_2'
     },
     {
         userId: '3',
-        userName: '띵준'
+        // userName: '띵준'
+        userName: 'data_3'
     },
     {
         userId: '4',
-        userName: '고밉'
+        // userName: '고밉'
+        userName: 'data_4'
     },
     {
         userId: '5',
-        userName: '낙낙'
+        // userName: '낙낙'
+        userName: 'data_5'
     },
     {
         userId: '6',
-        userName: '크리세리아'
+        // userName: '크리세리아'
+        userName: 'data_6'
     },
 ]
