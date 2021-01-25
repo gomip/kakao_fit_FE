e31952d8903732005fca618c30e8e9fb4fad06fa
gomip <rlawjdgns15@hotmail.com>
2021-01-25 23:50:20 +0900

    css 수정 및 info페이지 작성 시작

--- src/Info/InfoPage.tsx
+++ src/Info/InfoPage.tsx
@@ -2,18 +2,40 @@ import * as React from 'react'
 import {UserPage} from './UserPage'
 import './info.css'
 import {LogPage} from "./LogPage";
+import Select from 'react-select'
+import {UserData} from "./UserData";
 /**
  *  2021.01.14 | gomip | created
  */
 
+const {useState, useEffect} = React
+
+
+
 export const InfoPage: React.FC = () => {
     // State -----------------------------------------------------------------------------------------------------------
+    const [userOpt, setUserOpt] = useState([])
+    // LifeCycle -------------------------------------------------------------------------------------------------------
+    useEffect(() => {
+
+    }, [])
     // Function --------------------------------------------------------------------------------------------------------
     // Dom -------------------------------------------------------------------------------------------------------------
     return (
         <div className='info-wrapper'>
+            {/* 사용자 선택 시작*/}
+            <div className="user-list">
+                <div className="user-text">사용자</div>
+                <Select
+                    className="user-list-select"
+                />
+            </div>
+            {/* 사용자 선택 끝*/}
+
+            {/* 카드 정보 시작 */}
+            {/* 카드 정보 끝 */}
             {/* 사용자 정보 시작 */}
-            <UserPage />
+            {/*<UserPage />*/}
             {/* 사용자 정보 끝 */}
 
             {/*/!* 로그 시작 *!/*/}
